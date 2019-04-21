c1683e0d110f75258a1a097da40b96a7f5a82eb2
Lucas Savva <lucas@m1cr0man.com>
2019-04-21 12:40:54 +0100

    Fix dhcpd.conf patcher

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
+sed -i "s/DOMAINNAME/${DOMAINNAME}/g; s/DNSSERVERS/${DNSSERVERS}/g; s/NETPREFIX/${NETPREFIX}/g; s/RANGESTART/${RANGESTART}/g; s/RANGEEND/${RANGEEND}/g;" /etc/dhcpd.conf
+
 lighttpd -D -f /etc/lighttpd.conf &
 dhcpd -4 -f -d -cf /etc/dhcpd.conf &
 in.tftpd -L --secure /netboot/tftproot
